f8c8d216b8d73106e5c1d36f61397e099fca9331
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 09:28:19 +0500

    Add files via upload

---
+++ QNO20.ts
@@ -0,0 +1,14 @@
+// QNO:20
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+// let city=["karachi","lahore","quetta","peshawar"]
+// let mountains=["K2","lahore","egypt","kenya"]
+// let countries=["USA","india","pakistan","london"]
+// let languages=["English","spanish","urdu","sindhi"]
+// let rivers=["ravi river","indus river","Jehlum river","chenab ribver"]
+
+// console.log(`city,${city}`)
+// console.log(`mountain,${mountains}`)
+// console.log(`countries,${countries}`)
+// console.log(`languges,${languages}`)
+// console.log(`rivers,${rivers}`)
